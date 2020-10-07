2476fdc7fab6d0afb4c6acd482e66c61541d5c2d
shutaozhang <shutao76@uw.edu>
2020-10-06 23:08:01 -0700

    Binary Search using loop

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,6 +3,30 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
+
+        int low = 0;
+        int high = data.length-1;
+        int mid = 0;
+
+        while(high >= low) {
+
+            mid = low + (high - low) / 2;
+            //when Target is bigger than mid
+            if(target > data[mid]) low = mid + 1;
+            //when Target is smaller than mid
+            else if( target < data[mid]) high = mid - 1;
+            //when Target equals mid
+            else return mid;
+
+
+
+
+
+
+
+
+        }
+
         return -1; // Place holder
     }
 }
